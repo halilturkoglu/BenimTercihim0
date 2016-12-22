614a755c3ee0456a3410107f74f8b5fa22762714
Halil İbrahim TÜRKOĞLU <halilturkoglu@gmail.com>
2016-12-22 16:58:27 +0300

    YeniUye kayıt sayfası kodları girildi.

--- app/src/main/java/com/madlion/benimtercihim/YeniUye.java
+++ app/src/main/java/com/madlion/benimtercihim/YeniUye.java
@@ -1,7 +1,21 @@
 package com.madlion.benimtercihim;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by halilturkoglu on 14.12.2016.
@@ -11,5 +25,66 @@ public class YeniUye extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yeni_uye);
+
+        final RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        final String url=Genel_Islemler.siteadresi+"kayit.php";
+
+        Button btnUye=(Button) findViewById(R.id.btnUyeOl);
+        btnUye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                // Sonuç geldiğinde burada mesaj verilecek
+                                Toast.makeText(getBaseContext(),"İstek gönderildi. Gelen sonuç : "+response,Toast.LENGTH_LONG).show();
+                                if(response.contains("success"))
+                                {
+                                    //eğer sonuç olumlu ise anasayfaya geri dön.
+                                    Intent i=new Intent(YeniUye.this,MainActivity.class);
+                                    startActivity(i);
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //hata oluşursa mesaj burada verilecek
+                        Toast.makeText(getBaseContext(),"İstek gönderildi. Gelen sonuç : "+error.getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<String, String>();
+
+                        String adsoyad,kullaniciadi,eposta,sifre,sifre2;
+                        adsoyad=((EditText)findViewById(R.id.edtAdSoyad)).getText().toString();
+                        kullaniciadi=((EditText)findViewById(R.id.edtKullanici)).getText().toString();
+                        eposta=((EditText)findViewById(R.id.edtEposta)).getText().toString();
+                        sifre=((EditText)findViewById(R.id.edtSifre1)).getText().toString();
+                        sifre2=((EditText)findViewById(R.id.edtSifre2)).getText().toString();
+                        if(adsoyad!="" && kullaniciadi !="" && eposta!="" && sifre!="")
+                        {
+                            if(sifre.equals(sifre2))
+                            {
+                                params.put("ad_soyad",adsoyad);
+                                params.put("kullanici_adi",kullaniciadi);
+                                params.put("eposta",eposta);
+                                params.put("sifre",sifre);
+                            }
+                            else
+                                new AlertDialog.Builder(YeniUye.this).setTitle("Hatalı İşlem").setMessage("Girdiğiniz şifreler uyumlu değil").show();
+                        }
+
+                        return params;
+                    }
+                };
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
     }
 }

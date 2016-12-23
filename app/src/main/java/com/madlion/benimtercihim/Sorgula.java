38c1edc3589678965cf4a5269a36fe028d54da42
Halil İbrahim TÜRKOĞLU <halilturkoglu@gmail.com>
2016-12-23 11:54:51 +0300

    Sorgula dosyası için kodlar düzenlendi.

--- app/src/main/java/com/madlion/benimtercihim/Sorgula.java
+++ app/src/main/java/com/madlion/benimtercihim/Sorgula.java
@@ -1,7 +1,24 @@
 package com.madlion.benimtercihim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Sorgula extends AppCompatActivity {
 
@@ -9,5 +26,95 @@ public class Sorgula extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorgula);
+        Button btnSorgula;
+
+        btnSorgula=(Button) findViewById(R.id.btnSorgula);
+        btnSorgula.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String spnPuan;
+                final Integer edtMin, edtMak;
+                final float rtKira, rtYurt, rtYemek, rtAlisVeris, rtSehirSosyal, rtUniSosyal, rtIsImkani, rtUlasim, rtGuvenlik;
+
+                edtMin=Integer.valueOf(((EditText) findViewById(R.id.edtMin)).getText().toString());
+                edtMak=Integer.valueOf(((EditText) findViewById(R.id.edtMak)).getText().toString());
+
+                rtKira=((RatingBar) findViewById(R.id.rtKira)).getRating();
+                rtYurt=((RatingBar) findViewById(R.id.rtYurt)).getRating();
+                rtYemek=((RatingBar) findViewById(R.id.rtYemek)).getRating();
+                rtAlisVeris=((RatingBar) findViewById(R.id.rtAlisVeris)).getRating();
+                rtSehirSosyal=((RatingBar) findViewById(R.id.rtSehirSosyal)).getRating();
+                rtUniSosyal=((RatingBar) findViewById(R.id.rtUniSosyal)).getRating();
+                rtIsImkani=((RatingBar) findViewById(R.id.rtIsImkani)).getRating();
+                rtUlasim=((RatingBar) findViewById(R.id.rtUlasim)).getRating();
+                rtGuvenlik=((RatingBar) findViewById(R.id.rtGuvenlik)).getRating();
+
+                spnPuan=((TextView) ((Spinner) findViewById(R.id.spnPuan)).getChildAt(((Spinner) findViewById(R.id.spnPuan)).getSelectedItemPosition()).findViewById(R.id.textView)).getText().toString();
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, Genel_Islemler.siteadresi+"unisorgula.php",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                // Sonuç geldiğinde burada mesaj verilecek
+                                Toast.makeText(getBaseContext(),"İstek gönderildi. Gelen sonuç : "+response,Toast.LENGTH_LONG).show();
+                                if(response.contains("success"))
+                                {
+                                    //eğer sonuç olumlu ise uye sayfasına geri dön.
+                                    Intent i=new Intent(Sorgula.this,SorguRapor.class);
+//                                    i.putExtra("kullanici", k.getText().toString());
+                                    startActivity(i);
+
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //hata oluşursa mesaj burada verilecek
+                        Toast.makeText(getBaseContext(),"İstek gönderildi. Gelen sonuç : "+error.getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+
+                        //bu bilgiler post metodu ile PHP kanadına gönderilecek,
+                        //PHP kanadında SQL kodu oluşturulacak ve
+                        //sorgulama sonucunda elde edilen veriler geri döndürülecek.
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("bolum.puan", edtMin+":"+edtMak);
+                        params.put("sehir.kira", String.valueOf(rtKira));
+                        params.put("sehir.yurt", String.valueOf(rtYurt));
+                        params.put("sehir.yemek", String.valueOf(rtYemek));
+                        params.put("sehir.alisveris", String.valueOf(rtAlisVeris));
+                        params.put("sehir.sosyal", String.valueOf(rtSehirSosyal));
+                        params.put("uni.sosyal", String.valueOf(rtUniSosyal));
+                        params.put("bolum.isimkani", String.valueOf(rtIsImkani));
+                        params.put("sehir.ulasim", String.valueOf(rtUlasim));
+                        params.put("sehir.guvenlik", String.valueOf(rtGuvenlik));
+                        params.put("bolum.puanturu", spnPuan);
+                        return params;
+
+                        /*
+                        sql="select * from sehir, uni, bolum where "
+                                +"puan between "+edtMin+" and "+edtMak
+                                +" and sehir.kira>="+rtKira
+                                +" and sehir.yurt>="+rtYurt
+                                +" and sehir.yemek>="+rtYemek
+                                +" and sehir.alisveris>="+rtAlisVeris
+                                +" and sehir.sosyal>="+rtSehirSosyal
+                                +" and uni.sosyal>="+rtUniSosyal
+                                +" and bolum.isimkani>="+rtIsImkani
+                                +" and sehir.ulasim>="+rtUlasim
+                                +" and sehir.guvenlik>="+rtGuvenlik
+                                +" and bolum.puanturu='"+spnPuan+"'"
+                                +"";
+                                */
+
+                    }
+                };
+
+            }
+        });
     }
 }
